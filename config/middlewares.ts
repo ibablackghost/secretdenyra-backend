f0943230572bfe3f2fdd30974d9fe5a641f79e21
ibablackghost <papaibrahimadiagne2001@gmail.com>
2026-05-04 17:25:12 +0000

    ajout de l'admin

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,10 +1,39 @@
 import type { Core } from '@strapi/strapi';
 
+/** Origines du frontend Nyra (Vite). Ajoute ton domaine HTTPS en prod. */
+const corsOrigins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+];
+
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', ...corsOrigins],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'http://localhost:1337',
+            'http://127.0.0.1:1337',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: corsOrigins,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
